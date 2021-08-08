e67e634465a9bd488fb5f12dca8914def953a9a5
hijiri <hiji.k1124@gmail.com>
2021-08-09 01:39:22 +0900

    HeaderとFooterをAppへ反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Footer } from './components/molecules/footer';
+import { Header } from './components/molecules/header';
 import { Home } from './components/pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <switch>
         <Route path="/" component={Home} />
       </switch>
+      <Footer />
     </BrowserRouter>
   );
 }
