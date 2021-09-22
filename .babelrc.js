5ff20eea8f185d3610be881b0de8c7cf07c2e986
hijiri <hiji.k1124@gmail.com>
2021-09-23 02:08:53 +0900

    importの最適化(追記)

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,22 @@
 const plugins = [
-  ['babel-plugin-direct-import', { modules: ['@mui/material', '@mui/icons-material'] }],
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@mui/material',
+      libraryDirectory: '',
+      camel2DashComponentName: false,
+    },
+    'core',
+  ],
+  [
+    'babel-plugin-import',
+    {
+      libraryName: '@mui/icons-material',
+      libraryDirectory: '',
+      camel2DashComponentName: false,
+    },
+    'icons',
+  ],
 ];
 
 module.exports = { plugins };
