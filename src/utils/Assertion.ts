690eea9f291757e346bc2f591bb62374f7d37ab9
Hijiri <hiji.k1124@gmail.com>
2021-11-18 14:21:03 +0900

    forEachに置き換え

--- src/utils/Assertion.ts
+++ src/utils/Assertion.ts
@@ -2,23 +2,23 @@ import { Assertion } from '../models/QuestionDetailResponse';
 
 export const checkAssertion = (results: any[], code: string, assertions: Assertion[]) => {
   const failedAssertions: string[] = [];
-  for (let index = 0; index < assertions.length; index += 1) {
-    switch (assertions[index].assertion) {
+  assertions.forEach((assertion) => {
+    switch (assertion.assertion) {
       case "'+' in code":
         if (code.search(/\+/) === -1) {
-          failedAssertions.push(assertions[index].id);
+          failedAssertions.push(assertion.id);
         }
         break;
 
       case 'add(0, 0) === undefined':
         if (results.includes(undefined)) {
-          failedAssertions.push(assertions[index].id);
+          failedAssertions.push(assertion.id);
         }
         break;
       default:
         break;
     }
-  }
+  });
 
   return failedAssertions;
 };
