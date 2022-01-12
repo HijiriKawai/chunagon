d015963f679251226757562fe0c062f5ca8e3758
Hijiri <hiji.k1124@gmail.com>
2022-01-12 23:25:43 +0900

    sortをorganismsで行うように変更

--- src/components/organisms/QuestionList/index.tsx
+++ src/components/organisms/QuestionList/index.tsx
@@ -13,6 +13,9 @@ type QuestionListProps = {
 
 export const QuestionList: VFC<QuestionListProps> = (props: QuestionListProps) => {
   const { status, questions } = props;
+  questions.sort((a, b) => {
+    return a.level - b.level;
+  });
   const questionDetail = {
     OK: QuestionsBox,
     Failed: FailedBox,
